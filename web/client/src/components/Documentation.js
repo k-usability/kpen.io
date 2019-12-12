2a365c9fe53a3e60ff8dccfa2e89845328202dca
Suhabe Bugrara <suhabe@bugrara.com>
2019-12-11 21:27:19 -0800

    add videmo

--- web/client/src/components/Documentation.js
+++ web/client/src/components/Documentation.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, Fragment } from "react";
+import Vimeo from '@u-wave/react-vimeo';
 
 const Documentation = ({ match, history }) => {
     console.log(process.env.REACT_APP_API_URL)
 
 
-    return (<div></div>);
+    return (<div><Vimeo video="256677962"/></div>);
 };
 
 export default Documentation;
