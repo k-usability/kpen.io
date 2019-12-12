c82f59e64d747daea86b22b9190908c296cec379
Suhabe Bugrara <suhabe@bugrara.com>
2019-12-12 08:24:56 -0800

    documentation page

--- web/client/src/components/Documentation.js
+++ web/client/src/components/Documentation.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect, Fragment } from "react";
 import Vimeo from '@u-wave/react-vimeo';
+import "../App.css"
 
 const Documentation = ({ match, history }) => {
     console.log(process.env.REACT_APP_API_URL)
 
 
-    return (<div><Vimeo video="256677962"/></div>);
+
+    return (<div className="documentation">
+        <p>1. Watch the <a href="https://vimeo.com/379056987">tutorial video</a> for a quick walkthrough of this site.</p>
+        <p>2. Read through <a href="https://kspec.io">kspec.io</a> to see examples of K-YAML specifications.</p>
+        <p>3. Read the <a href="https://arxiv.org/abs/1912.02951">technical report</a>.</p>
+        <p>4. Subscribe to the mailing list.</p>
+        <br/>
+        <br/>
+        <br/>
+        </div>
+        );
 };
 
 export default Documentation;
